10b817bddc7245cc5d70d5c4e43112a597ff8760
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2023-09-02 11:58:41 +0600

    Added sort by view functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Global scope
+let getAllDataFromApi;
 
 // Fetch data from category API
 let fetchCategoryButtons = async () => {
@@ -37,6 +38,7 @@ let fetchVideoInfo = async (id) => {
     .then((data) => videos.push(data));
 
   displayVideoInfo(videos);
+  getAllDataFromApi = videos;
 };
 
 fetchVideoInfo(1000);
@@ -48,13 +50,11 @@ let displayVideoInfo = (video) => {
   let videoInfo = video[0].data;
 
   if (videoInfo.length <= 0) {
-    let notFound = document.getElementById("not-found");
-    notFound.classList.remove("hidden");
-    notFound.innerHTML = `    
+    cardsContainer.innerHTML = `    
     <section class="not-found">
     <div class="nf-container w-[90%] m-auto">
       <img
-        class="w-[20%] md:w-[15%] lg:w-[15%] m-auto"
+        class="w-[100%] m-auto"
         src="./images/Icon.png"
         alt=""
       />
@@ -122,7 +122,68 @@ let displayVideoInfo = (video) => {
 
 // Sort videos upon clicking a button
 let sortByView = () => {
-  console.log("hi");
+  let views = getAllDataFromApi[0].data;
+  let allViews = [];
+  views.forEach((item) => {
+    allViews.push(item);
+  });
+
+  let descOrder = allViews.sort(
+    (a, b) => parseFloat(b.others.views) - parseFloat(a.others.views)
+  );
+
+  let cardsContainer = document.getElementById("cards-container");
+  cardsContainer.textContent = "";
+  descOrder.forEach((item) => {
+    let card = document.createElement("div");
+
+    card.innerHTML = `
+    <div class="card bg-base-100 shadow-xl">
+    <figure class="cdx">
+      <img src=${item.thumbnail} alt="Shoes" class="rounded-xl h-[170px] w-[100%] " />
+    
+    </figure>
+    <div class="info my-9 flex gap-5 mx-3">
+      <img
+        class="w-[50px] h-[50px] rounded-full"
+        src=${item.authors[0].profile_picture}
+        alt=""
+      />
+      <div class="metadata">
+        <p class="font-bold text-[18px] mb-1">
+          ${item.title}
+        </p>
+        <div class="owner-info flex items-center gap-4 mb-2">
+          <p>${item.authors[0].profile_name}</p>
+          <div></div>
+
+        </div>
+        <p>${item.others.views} views</p>
+      </div>
+    </div>
+  </div>
+    `;
+
+    if (item.authors[0].verified) {
+      let iconElement = document.createElement("i");
+      iconElement.className = "fa-solid fa-circle-check";
+      iconElement.style.color = "#2568EF";
+      card.querySelector(".owner-info div").appendChild(iconElement);
+    }
+
+    if (item.others.posted_date) {
+      let totalMinutes = item.others.posted_date;
+      let hours = Math.floor(totalMinutes / 60);
+      let minutes = totalMinutes % 60;
+      let time = document.createElement("div");
+      time.className =
+        "absolute right-5 top-[38%] bg-black text-[#fff] p-2 text-[12px] rounded-lg";
+      time.innerHTML = `${hours}hrs ${minutes}mins ago`;
+      card.querySelector(".cdx").appendChild(time);
+    }
+
+    cardsContainer.appendChild(card);
+  });
 };
 
 // Redirect to blog page
